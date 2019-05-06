057e28d713dca951ccb0822b120d9a627627f70f
Akash Patel <akash.abap@yahoo.com>
2019-05-06 08:05:22 -0400

    Setup repo for feching data

--- app/src/main/java/com/akash/a500px/MainActivity.kt
+++ app/src/main/java/com/akash/a500px/MainActivity.kt
@@ -2,6 +2,7 @@ package com.akash.a500px
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.akash.a500px.networking.PhotoRepo
 
 class MainActivity : AppCompatActivity() {
 
